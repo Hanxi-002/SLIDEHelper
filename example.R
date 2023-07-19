c8979ab6e018db7e82730b09b032dd0fa0dca718
Hanxi Xiao <xiaoh@pitt.edu>
2023-07-19 16:39:26 -0400

    added control plot function

--- example.R
+++ example.R
@@ -1,6 +1,7 @@
 library(SLIDEHelper)
 library(tidyverse)
 library(doParallel)
+library(ggplot2)
 
 cores <-  as.numeric(Sys.getenv('SLURM_CPUS_PER_TASK', unset=NA))
 if(is.na(cores)) cores <- detectCores()
@@ -11,7 +12,7 @@ cat('number of cores using', cores, '. . .\n')
 
 er_path <- "final_delta_0.001_lambda_0.5.rds"
 x_path <- "x.csv"
-out_path <- "/Users/xiaoh/Desktop/toy"
+out_path <- "output/"
 Z_matrix <- CalcZMatrix(x_path, er_path, out_path)
 
 
@@ -22,12 +23,15 @@ SLIDE_res <- runSLIDE(y_path = y_path,
                       z_matrix = Z_matrix,
                       er_path = er_path,
                       do_interacts = TRUE,
-                      spec = 0.5,
+                      spec = 0.2,
                       niter = 100)
 
 num_top_feats <- 10
-condition <- "corr"
+condition <- "auc"
 SLIDE_res <- GetTopFeatures(x_path, y_path, er_path, out_path, SLIDE_res, num_top_feats = 10, condition)
 
 plotSigGenes(SLIDE_res, out_path, plot_interaction = TRUE)
+
+#the SLIDE_res has to be the output from GetTopFeatures
+CalcControlPerformance(z_matrix = Z_matrix, y_path, SLIDE_res, niter = 1000, condition, out_path)
 
